568de73c7188ce63940c49ce6805e8e2cfa8a41d
joozd <euroshopper@gmail.com>
2026-09-25 17:13:57 +0200

    Fixed deserialzing and multiple deltas for same version

--- src/main/kotlin/nl/joozd/airportsource/io/writeAirportDataAsGzipJson.kt
+++ src/main/kotlin/nl/joozd/airportsource/io/writeAirportDataAsGzipJson.kt
@@ -3,6 +3,7 @@ package nl.joozd.airportsource.io
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.encodeToStream
+import nl.joozd.airportsource.yasinterfaces.AirportData
 import java.nio.file.Path
 import java.security.DigestOutputStream
 import java.security.MessageDigest
@@ -11,20 +12,20 @@ import kotlin.io.path.fileSize
 import kotlin.io.path.outputStream
 
 /**
- * Serializes [serializable] as JSON, compresses it with GZIP, and writes it to [target].
+ * Serializes [airportData] as JSON, compresses it with GZIP, and writes it to [target].
  *
  * @return Metadata describing the compressed file that was written.
  */
 @ExperimentalSerializationApi
-inline fun <reified T> writeSerializableAsGzipJson(
-    serializable: T,
+internal fun writeAirportDataAsGzipJson(
+    airportData: AirportData,
     target: Path,
 ): WriteResult {
     val digest = MessageDigest.getInstance("SHA-256")
 
     DigestOutputStream(target.outputStream(), digest).use { digestOutput ->
         GZIPOutputStream(digestOutput).use { gzipOutput ->
-            Json.encodeToStream(serializable, gzipOutput)
+            Json.encodeToStream(airportData, gzipOutput)
         }
     }
 
